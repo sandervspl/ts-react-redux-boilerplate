37181093c6ff91ffd3e7d1e6f8baf54a147d1066
Sander Vispoel <sandervispoel@hotmail.com>
2018-03-15 12:46:16 +0100

    Fix filename not matching for dev server

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     ],
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
+        filename: 'bundle.js',
         publicPath: '/dist/',
     },
     module: {
