25d7b1b238365932fdf0a5ab0978e3793c4415f9
Sander Vispoel <sandervispoel@hotmail.com>
2018-03-20 23:35:22 +0100

    Fix stylesheet resource type

--- src/server/helpers/renderFullPage.ts
+++ src/server/helpers/renderFullPage.ts
@@ -9,7 +9,7 @@ module.exports = ({ html, styleTags }: RenderOptions): string => `
     <title>React Redux Boilerplate</title>
     <link rel="shortcut icon" href="/static/favicon.ico" />
     ${styleTags || ''}
-    ${html ? '<link rel="stylesheet" type="text/css" href="/style.css">' : ''}
+    ${html ? '<link rel="stylesheet" type="text/html" href="/style.css">' : ''}
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 
