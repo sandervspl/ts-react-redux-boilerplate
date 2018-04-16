cab883d4b45dc7a2c7d75aca6a2de5fe16840b81
Sander Vispoel <sandervispoel@hotmail.com>
2018-04-16 21:24:37 +0200

    Declare all global variable types

--- src/config/declarations.d.ts
+++ src/config/declarations.d.ts
@@ -1,6 +1,9 @@
 // global variables
 declare const __DEV__: boolean;
+declare const __PROD__: boolean;
+declare const __ACC__: boolean;
 declare const __CLIENT__: boolean;
+declare const __SERVER__: boolean;
 
 // extend window object
 interface Window {
