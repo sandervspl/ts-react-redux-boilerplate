525052f2dde98d4ac5c975bc64043ad1981a3d21
Sander <sander.vispoel@labela.nl>
2019-01-05 22:02:56 +0100

    Update config files

--- src/config/globals.ts
+++ src/config/globals.ts
@@ -1,18 +1,16 @@
 import { port } from './index';
 
 const env = process.env.NODE_ENV || 'development';
-const appEnv = process.env.APP_ENV || 'development';
 
 export default (type: string) => ({
   'process.env': {
     NODE_ENV: JSON.stringify(env),
-    APP_ENV: JSON.stringify(appEnv),
-    PORT: port,
+    PORT: port.client,
   },
-  __DEV__: appEnv === 'development',
-  __TEST__: appEnv === 'test',
-  __PROD__: appEnv === 'production',
-  __ACC__: appEnv === 'acceptation',
+  __DEV__: env === 'development',
+  __TEST__: env === 'test',
+  __PROD__: env === 'production',
+  __ACC__: env === 'acceptation',
   __CLIENT__: type === 'client',
   __SERVER__: type === 'server',
 });
