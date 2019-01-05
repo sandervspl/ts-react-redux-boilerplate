e349af33d7a0c6b919bf465c74c2f520be64bc8a
Sander <sander.vispoel@labela.nl>
2019-01-05 21:57:17 +0100

    Update babelrc

---
+++ .babelrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    presets: [
+        '@babel/env',
+        '@babel/typescript',
+        '@babel/react',
+    ],
+    plugins: [
+        'lodash',
+        'babel-plugin-styled-components',
+        'react-loadable/babel',
+    ],
+}
