151111077f79b30f822658b710a9f8e33af485a9
Sander <sander.vispoel@labela.nl>
2019-01-06 17:13:32 +0100

    Update Button with new type support

--- src/app/components/common/Button.tsx
+++ src/app/components/common/Button.tsx
@@ -1,27 +1,20 @@
-import * as i from 'app/interfaces';
+import * as i from 'types';
 import * as React from 'react';
-import styled from 'styled-components';
-import { StyledComponent } from 'app/services';
+import styled from '@styled-components';
 
-const Button = styled(StyledComponent<ButtonProps>('button'))`
-    background: ${props => props.theme.color.primary};
-    padding: 10px 20px;
-    border: none;
-    color: ${props => props.theme.color.white};
-    font-size: 16px;
-    outline: none;
-    border-radius: 5px;
-    cursor: pointer;
-    font-family: ${props => props.theme.font.futura};
+const Button = styled.button<ButtonProps>`
+  background: ${(props) => props.theme.color.primary};
+  padding: 10px 20px;
+  border: none;
+  color: ${(props) => props.theme.color.white};
+  font-size: 16px;
+  outline: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-family: ${(props) => props.theme.font.futura};
 `;
 
-// PropTypes as interface
-export interface ButtonProps extends i.BaseStyled, React.ButtonHTMLAttributes<HTMLButtonElement> {}
-
-// default props with interface as blueprint for this object
-const defaultProps: ButtonProps = {};
-
-// apply default props to Button
-Button.defaultProps = defaultProps;
+// Use type for easier and composition
+export type ButtonProps = i.BaseStyled & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export default Button;
